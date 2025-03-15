6df4282d02bbe617427f063286e4f47b76eeac3c
bstephe9 <bstephenson9807@gmail.com>
2025-03-15 17:54:32 -0400

    08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,52 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (A) {
+  let sum = 0;
+
+  for (let i = 0; i < A.length; i++) {
+    sum += A[i];
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function (A) {
+  if (A.length == 0)
+    return 0;
 
+  let result = 1;
+
+  for (let i = 0; i < A.length; i++) {
+    result *= A[i];
+  }
+
+  return result;
 };
 
-const power = function() {
-	
+const power = function (x, y) {
+  let result = x;
+
+  for (let i = 1; i < y; i++) {
+    result *= x;
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (x) {
+  let result = 1;
+
+  for (let i = x; i > 0; i--) {
+    result *= i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
@@ -31,3 +58,5 @@ module.exports = {
   power,
   factorial
 };
+
+console.log(power(4, 3));
