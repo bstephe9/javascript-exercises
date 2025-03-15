b2d07b7f4bb301793e072484dd2f2a8ec09bfa88
bstephe9 <bstephenson9807@gmail.com>
2025-03-15 17:34:09 -0400

    07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (x) {
+  // x °F ≘ (x − 32) × ⁠5/9⁠ °C 
+  let result = (x - 32) * (5 / 9);
+  return Math.round(result * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (x) {
+  // x °C ≘ (x × ⁠9/5⁠ + 32) °F 
+  let result = x * (9 / 5) + 32;
+  return Math.round(result * 10) / 10;
 };
 
 // Do not edit below this line
