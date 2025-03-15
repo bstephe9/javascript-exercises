45d61c4072767d65851465da3599d4730f10f1f6
bstephe9 <bstephenson9807@gmail.com>
2025-03-15 15:36:49 -0400

    05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (x, y) {
+  if (x < 0 || y < 0) {
+    return "ERROR";
+  }
 
+  if (!Number.isInteger(x) || !Number.isInteger(y)) {
+    return "ERROR";
+  }
+
+  // Allows function to work with larger numbers first
+  if (x > y) {
+    [x, y] = [y, x];
+  }
+
+  let sum = 0;
+
+  for (let i = x; i <= y; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
